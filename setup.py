fb81ac83e3f27e40be73d0ae2c1d590457f84bb3
Zac-Milioli <zacmilioli@gmail.com>
2023-10-24 11:28:36 -0300

    Imports e variáveis adicionados ao setup

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
+# Aqui dentro são criadas as variáveis a serem usadas pelo sistema, além de serem feitos os imports
+# e a instalação/atualização de pacotes
+
+# Imports
+import pandas as pd
+import googlemaps
+from places import *
+from routes import *
 
 # Paths
 path_setup = r'system/has_setup.txt'
+path_input = r'input/'
+path_output = r'output/'
 
+
+# Instalador/Atualizador de pacotes
 def install_packages():
     has_setup = open(path_setup, 'r').read()
     if has_setup == '0':
@@ -11,4 +23,3 @@ def install_packages():
         os.system('pip install pandas')
         os.system('pip install googlemaps')
         has_setup = open(path_setup, 'w').write('1')
-
