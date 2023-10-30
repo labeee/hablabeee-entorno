bed91a818d24cf788058efd6b7ba7acedc992100
Zac-Milioli <zacmilioli@gmail.com>
2023-10-30 11:56:35 -0300

    Renomeação de variável teste

--- testingPlayground/request_experimental.py
+++ testingPlayground/request_experimental.py
@@ -1,17 +1,19 @@
-coordenadas = '-25.997858098017268/-52.22127528495758' # Atualmente são coordenadas de Covó, em Mangueirinha (PR)
+import pandas as pd
+import googlemaps
+# Atualmente são coordenadas de Covó, em Mangueirinha (PR)
+coordenadas = '-25.997858098017268/-52.22127528495758'
+
 coordinates = coordenadas.split('/')
 key = 'INSIRA SUA CHAVE'
 
-import googlemaps
-import pandas as pd
 
 client = googlemaps.Client(key)
 
 response = client.places_nearby(
-                location=(coordinates[0], coordinates[1]),
-                keyword='Mercado',
-                rank_by='distance'
-            )
+    location=(coordinates[0], coordinates[1]),
+    keyword='Mercado',
+    rank_by='distance'
+)
 
 print(response)
 
@@ -19,8 +21,8 @@ with open(r'testingPlayground/Teste_de_Retorno.txt', 'w') as pn:
     pn.write('\n\n'.join(str(response).split("business_status")))
     pn.close()
 
-pontonemo = pd.DataFrame(response.get('results'))
+dataframe = pd.DataFrame(response.get('results'))
 
-print(pontonemo)
+print(dataframe)
 
-pontonemo.to_csv(r'testingPlayground/DataFrame_de_Teste.csv', sep=';')
+dataframe.to_csv(r'testingPlayground/DataFrame_de_Teste.csv', sep=';')
